5135031bcbdaed0501175213a952f03b8a087907
fcoprata <fcopratan@gmail.com>
2022-04-10 13:19:24 -0300

    update code

--- ChatBOT/bot.py
+++ ChatBOT/bot.py
@@ -5,6 +5,12 @@ class Bot(DesktopBot):
 
         self.execute("C:/Users/fcopr/AppData/Local/WhatsApp/WhatsApp.exe")
 
+        if not self.find( "search", matching=0.97, waiting_time=30000):
+            self.not_found("search")
+        self.click()
+
+        self.paste("Thiago Viana")
+
         if not self.find( "user", matching=0.97, waiting_time=30000):
             self.not_found("user")
         self.click()
@@ -18,6 +24,11 @@ class Bot(DesktopBot):
         if not self.find("send", matching=0.97, waiting_time=10000):
             self.not_found("send")
         self.click()
+
+        self.alt_f4()
+
+        self.execute("C:/Users/fcopr/AppData/Local/WhatsApp/WhatsApp.exe")
+
 
     def not_found(self, label):
         print(f"Element not found: {label}")
